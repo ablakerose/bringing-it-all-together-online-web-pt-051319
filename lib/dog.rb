193243bb76f7dafae26ca96c8ecab0a9cb126bb6
Blake Rose <bdecktor@gmail.com>
2019-08-02 00:29:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,6 +47,23 @@ class Dog
         DB[:conn].execute(sql)
   end
 
+  def self.create(hash_of_attributes)
+        dog = self.new(hash_of_attributes)
+        dog.save
+
+        dog
+    end
+
+    def self.find_by_id(id)
+      sql = <<-SQL
+        SELECT * FROM dogs WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, id).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
+
   def self.find_or_create_by(name:, breed:)
       dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
       if !dog.empty?
