c93d7ae4584082272dd60d61392899821eb19c2e
Blake Rose <bdecktor@gmail.com>
2019-08-02 00:34:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -89,7 +89,7 @@ class Dog
       SELECT * FROM dogs WHERE name = ?
     SQL
 
-    DB[:conn].execute(sql, id).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
