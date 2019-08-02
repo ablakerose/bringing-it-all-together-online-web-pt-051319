3eee2400dda4af6074eb613eeff56cff6ce06e36
Blake Rose <bdecktor@gmail.com>
2019-08-02 00:06:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,6 +10,14 @@ class Dog
 
 
   def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
