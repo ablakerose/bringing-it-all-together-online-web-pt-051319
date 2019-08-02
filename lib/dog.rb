9b9d149b2e0322efd6d6ac72ca8b80e6fc47663c
Blake Rose <bdecktor@gmail.com>
2019-08-02 00:31:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,10 +58,11 @@ class Dog
       sql = <<-SQL
         SELECT * FROM dogs WHERE id = ?
       SQL
-binding.pry
+
       DB[:conn].execute(sql, id).map do |row|
         self.new_from_db(row)
       end.first
+      binding.pry
     end
 
   def self.find_or_create_by(name:, breed:)
