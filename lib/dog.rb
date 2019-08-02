959bd0000eafd3fe4a87b2082849ddbe771d78b9
Blake Rose <bdecktor@gmail.com>
2019-08-02 00:18:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,8 @@ class Dog
   attr_accessor :name, :breed, :id
 
   def initialize(attributes)
-  
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+      self.id ||= nil
   end
 
   def save
