a143b4fdb804e02465a699c1db7c760445e52df9
Blake Rose <bdecktor@gmail.com>
2019-08-02 00:28:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,7 +26,6 @@ class Dog
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    binding.pry
   end
 
   def self.create_table
